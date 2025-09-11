b27e76ea2f8194cf5e2f39e54e7f0fad83b1e30c
scuadros248 <scuadros248@unab.edu.co>
2025-09-11 12:44:24 -0500

    Layouts basicos

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Listado de productos</h1>
-</body>
-
-</html>
+@section('content')
+    <h1>LisPtado de productos</h1>
+@endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
+@section('content')
     <h1>PRODUCT DETAILS:</h1>
-</body>
-
-</html>
+    <p>Aquí van los detalles del producto</p>
+@endsection
