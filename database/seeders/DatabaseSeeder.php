d76896d9d68314082e1d8ea89c05c5200b5a2cfa
sergio <scuadros248@unab.edu.co>
2025-10-02 12:18:25 -0500

    Factory Products
    fix: syntax 2
    
    fix: syntax

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -24,5 +23,6 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             BrandSeeder::class,
         ]);
+        Product::factory(1000)->create;
     }
 }
